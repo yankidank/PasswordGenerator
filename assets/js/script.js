e8a5615d43b21a97ed1d69e856ba94ea050b2550
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-01-16 08:08:12 -0800

    Added basic html generator function. Still needs configurable options added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-function passField (){
+function passCompute (length){
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+//console.log(passCompute(5));
 
-}
 function passGenerate (){
-	
+  var text = document.getElementById('passField');
+  text.value = passCompute(5);
 }
+
 function passCopy (){
   /* Get the text field */
   var copyText = document.getElementById("passField");
